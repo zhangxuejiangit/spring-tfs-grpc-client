5e19caae4e4d258d2453c120c63223c719e2a6d1
zhangxuejiangit <zhangxuejian@outlook.com>
2024-08-28 21:46:58 +0800

    update

---
+++ src/main/java/com/example/springgrpcclient/SpringHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgrpcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHelloworldApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springgrpcclient/controller/Hello.java
@@ -0,0 +1,14 @@
+package com.example.springgrpcclient.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Hello {
+
+    @RequestMapping("/world")
+    public String world() {
+        return "Hello World!";
+    }
+}
