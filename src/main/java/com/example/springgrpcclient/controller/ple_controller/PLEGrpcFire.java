b9ff6974738a88e7a7c590c3c8eba1bb56676689
zhangxuejiangit <zhangxuejian@outlook.com>
2024-10-12 16:40:16 +0800

    add ple model clients

--- src/main/java/com/example/springgrpcclient/controller/ple_controller/PLEGrpcFire.java
+++ src/main/java/com/example/springgrpcclient/controller/ple_controller/PLEGrpcFire.java
@@ -2,6 +2,7 @@ package com.example.springgrpcclient.controller.ple_controller;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.tensorflow.framework.DataType;
@@ -12,9 +13,6 @@ import tensorflow.serving.Predict;
 import tensorflow.serving.PredictionServiceGrpc;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 
 @RestController
 @RequestMapping("/ple/grpc")
@@ -53,109 +51,120 @@ public class PLEGrpcFire {
         modelSpecBuilder.setSignatureName("serving_default");
         System.out.println("step i3: init the model successfully");
 
-        init_feature_map();
+        init_feature_map(100);
     }
 
-    private void init_feature_map() {
+    private void init_feature_map(int n) {
         featureMap = new HashMap<>();
 
-        featureMap.put("feedback_num_7d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("uv_rate_15d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("ctr_7d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("uv_rate_7d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("view_num_sqrt", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("user_genres_7d", createIntTensorProto(Arrays.asList(1, 1, 1)));
-        featureMap.put("user_directors_7d", createIntTensorProto(Arrays.asList(1, 1, 1)));
-        featureMap.put("item_languages", createIntTensorProto(Arrays.asList(1, 1)));
-        featureMap.put("user_publishers_7d", createIntTensorProto(Arrays.asList(1, 1, 1)));
-        featureMap.put("view_num_square", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("uv_rate_30d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("view_num_norm", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("download_num_30d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("view_num_15d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("streaming_num_15d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("feedback_rate_1d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("download_rate_1d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("item_publisher", createIntTensorProto(Arrays.asList(1)));
-        featureMap.put("ctr_30d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("item_directors", createIntTensorProto(Arrays.asList(1, 1, 1)));
-        featureMap.put("user_actors_7d", createIntTensorProto(Arrays.asList(1, 1, 1)));
-        featureMap.put("phone_brand", createIntTensorProto(Arrays.asList(1)));
-        featureMap.put("feedback_num_1d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("ctr_15d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("user_languages_7d", createIntTensorProto(Arrays.asList(1, 1)));
-        featureMap.put("item_id", createIntTensorProto(Arrays.asList(1)));
-        featureMap.put("view_num_7d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("item_actors", createIntTensorProto(Arrays.asList(1, 1, 1)));
-        featureMap.put("feedback_rate_7d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("release_at_log", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("category", createIntTensorProto(Arrays.asList(1)));
-        featureMap.put("release_at_sqrt", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("release_at_norm", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("avg_playtime_7d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("feedback_num_15d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("view_num_log", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("ip_city", createIntTensorProto(Arrays.asList(1)));
-        featureMap.put("streaming_num_7d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("download_num_7d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("item_genres", createIntTensorProto(Arrays.asList(1, 1, 1)));
-        featureMap.put("download_num_1d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("user_source", createIntTensorProto(Arrays.asList(1)));
-        featureMap.put("release_at_square", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("view_num_30d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("streaming_num_1d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("avg_playtime_15d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("avg_playtime_30d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("feedback_rate_30d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("phone_model", createIntTensorProto(Arrays.asList(1)));
-        featureMap.put("ip_state", createIntTensorProto(Arrays.asList(1)));
-        featureMap.put("entrance", createIntTensorProto(Arrays.asList(1)));
-        featureMap.put("streaming_num_30d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("download_rate_30d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("download_rate_7d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("ctr_1d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("download_num_15d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("view_num_1d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("feedback_rate_15d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("feedback_num_30d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("avg_playtime_1d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("user_id", createIntTensorProto(Arrays.asList(1)));
-        featureMap.put("download_rate_15d", createFloatTensorProto(Arrays.asList(0.0f)));
-        featureMap.put("uv_rate_1d", createFloatTensorProto(Arrays.asList(0.0f)));
+        featureMap.put("feedback_num_7d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("uv_rate_15d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("ctr_7d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("uv_rate_7d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("view_num_sqrt", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("user_genres_7d", createIntTensorProto(Arrays.asList(1, 1, 1), n));
+        featureMap.put("user_directors_7d", createIntTensorProto(Arrays.asList(1, 1, 1), n));
+        featureMap.put("item_languages", createIntTensorProto(Arrays.asList(1, 1), n));
+        featureMap.put("user_publishers_7d", createIntTensorProto(Arrays.asList(1, 1, 1), n));
+        featureMap.put("view_num_square", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("uv_rate_30d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("view_num_norm", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("download_num_30d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("view_num_15d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("streaming_num_15d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("feedback_rate_1d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("download_rate_1d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("item_publisher", createIntTensorProto(Arrays.asList(1), n));
+        featureMap.put("ctr_30d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("item_directors", createIntTensorProto(Arrays.asList(1, 1, 1), n));
+        featureMap.put("user_actors_7d", createIntTensorProto(Arrays.asList(1, 1, 1), n));
+        featureMap.put("phone_brand", createIntTensorProto(Arrays.asList(1), n));
+        featureMap.put("feedback_num_1d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("ctr_15d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("user_languages_7d", createIntTensorProto(Arrays.asList(1, 1), n));
+        featureMap.put("item_id", createIntTensorProto(Arrays.asList(1), n));
+        featureMap.put("view_num_7d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("item_actors", createIntTensorProto(Arrays.asList(1, 1, 1), n));
+        featureMap.put("feedback_rate_7d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("release_at_log", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("category", createIntTensorProto(Arrays.asList(1), n));
+        featureMap.put("release_at_sqrt", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("release_at_norm", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("avg_playtime_7d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("feedback_num_15d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("view_num_log", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("ip_city", createIntTensorProto(Arrays.asList(1), n));
+        featureMap.put("streaming_num_7d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("download_num_7d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("item_genres", createIntTensorProto(Arrays.asList(1, 1, 1), n));
+        featureMap.put("download_num_1d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("user_source", createIntTensorProto(Arrays.asList(1), n));
+        featureMap.put("release_at_square", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("view_num_30d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("streaming_num_1d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("avg_playtime_15d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("avg_playtime_30d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("feedback_rate_30d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("phone_model", createIntTensorProto(Arrays.asList(1), n));
+        featureMap.put("ip_state", createIntTensorProto(Arrays.asList(1), n));
+        featureMap.put("entrance", createIntTensorProto(Arrays.asList(1), n));
+        featureMap.put("streaming_num_30d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("download_rate_30d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("download_rate_7d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("ctr_1d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("download_num_15d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("view_num_1d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("feedback_rate_15d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("feedback_num_30d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("avg_playtime_1d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("user_id", createIntTensorProto(Arrays.asList(1), n));
+        featureMap.put("download_rate_15d", createFloatTensorProto(Arrays.asList(0.0f), n));
+        featureMap.put("uv_rate_1d", createFloatTensorProto(Arrays.asList(0.0f), n));
 
 
         System.out.println("step i4: init the feature map successfully");
         System.out.println(featureMap.toString());
     }
 
-    private TensorProto createIntTensorProto(List<Integer> collections) {
-        return TensorProto.newBuilder()
+    private TensorProto createIntTensorProto(List<Integer> collections, int n) {
+        TensorProto.Builder builder = TensorProto.newBuilder()
                 .setDtype(DataType.DT_INT32)
-                .addAllIntVal(collections)
                 .setTensorShape(
                         TensorShapeProto.newBuilder()
                                 .addDim(org.tensorflow.framework.TensorShapeProto.Dim.newBuilder().setSize(1))
                                 .addDim(org.tensorflow.framework.TensorShapeProto.Dim.newBuilder().setSize(collections.size()))
                                 .build()
-                )
-                .build();
+                );
+        for (int i = 0; i < n; i++) {
+            builder
+                    .addAllIntVal(collections);
+        }
+        return builder.build();
     }
 
-    private TensorProto createFloatTensorProto(List<Float> collections) {
-        return TensorProto.newBuilder()
+    private TensorProto createFloatTensorProto(List<Float> collections, int n) {
+        TensorProto.Builder builder = TensorProto.newBuilder()
                 .setDtype(DataType.DT_FLOAT)
-                .addAllFloatVal(collections)
                 .setTensorShape(
                         TensorShapeProto.newBuilder()
                                 .addDim(org.tensorflow.framework.TensorShapeProto.Dim.newBuilder().setSize(1))
                                 .addDim(org.tensorflow.framework.TensorShapeProto.Dim.newBuilder().setSize(collections.size()))
                                 .build()
-                )
-                .build();
+                );
+        for (int i = 0; i < n; i++) {
+            builder
+                    .addAllFloatVal(collections);
+        }
+        return builder.build();
     }
 
     @RequestMapping("fire")
     public String fire() {
+
+        int times = 1000;
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start(String.format("%s times requests", times));
 
         System.out.println("step 1: build the request begin...");
         // build request
@@ -166,16 +175,25 @@ public class PLEGrpcFire {
                 .putAllInputs(featureMap)
                 .build();
 
+
         System.out.println("Printing request \n" + request.toString());
         System.out.println("step 1: build the request successfully");
 
         System.out.println("step 2: init the predict begin...");
         // run predict
-        Predict.PredictResponse response = stub.predict(request);
+        Predict.PredictResponse response = Predict.PredictResponse.newBuilder().build();
+        for (int i = 0; i < times; i++) {
+             response = stub.predict(request);
+        }
+
         System.out.println(response.toString());
         System.out.println("step 2: fire the predict successfully");
 
+        stopWatch.stop();
+
+        System.out.println(stopWatch.prettyPrint());
         return "all done!";
+
     }
 
 }
