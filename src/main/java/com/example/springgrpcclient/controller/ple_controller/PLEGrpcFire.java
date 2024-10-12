44a2d29f29783911f7d85bbe65fe44084cfb6be3
zhangxuejiangit <zhangxuejian@outlook.com>
2024-10-12 14:57:53 +0800

    add ple model clients

--- src/main/java/com/example/springgrpcclient/controller/ple_controller/PLEGrpcFire.java
+++ src/main/java/com/example/springgrpcclient/controller/ple_controller/PLEGrpcFire.java
@@ -21,86 +21,75 @@ public class PLEGrpcFire {
     String modelName = "ple";
     //long modelVersion = 123;
 
-    @RequestMapping("fire")
-    public String fire() {
+    private ManagedChannel channel;
 
-        System.out.println("step 1: init the channel begin...");
-        ManagedChannel channel = ManagedChannelBuilder
+    private PredictionServiceGrpc.PredictionServiceBlockingStub stub;
+
+    private Model.ModelSpec.Builder modelSpecBuilder;
+
+    public PLEGrpcFire() {
+        System.out.println("step i1: init the channel begin...");
+        channel = ManagedChannelBuilder
                 .forAddress(host, port)
                 .usePlaintext()
                 .build();
-        System.out.println("step 1: init the channel successfully");
+        System.out.println("step i1: init the channel successfully");
 
-        System.out.println("step 2: init the stub begin...");
-        PredictionServiceGrpc.PredictionServiceBlockingStub stub = PredictionServiceGrpc.newBlockingStub(channel);
-        System.out.println("step 2: init the stub successfully");
+        System.out.println("step i2: init the stub begin...");
+        stub = PredictionServiceGrpc.newBlockingStub(channel);
+        System.out.println("step i2: init the stub successfully");
 
-        System.out.println("step 3: init the predict request begin...");
-        // create PredictRequest
-        Predict.PredictRequest.Builder requestBuilder = Predict.PredictRequest.newBuilder();
-        System.out.println("step 3: init the predict request successfully");
-
-        System.out.println("step 4: init the model begin...");
+        System.out.println("step i3: init the model begin...");
         // create ModelSpec
-        Model.ModelSpec.Builder modelSpecBuilder = Model.ModelSpec.newBuilder();
+        modelSpecBuilder = Model.ModelSpec.newBuilder();
         modelSpecBuilder.setName(modelName);
         //modelSpecBuilder.setVersion(Int64Value.of(modelVersion));
         modelSpecBuilder.setSignatureName("serving_default");
-        System.out.println("step 4: init the model successfully");
+        System.out.println("step i3: init the model successfully");
+    }
 
-        System.out.println("step 5: set the model into request begin...");
-        // set model for request
-        requestBuilder.setModelSpec(modelSpecBuilder);
-        System.out.println("step 5: set the model into request successfully");
-
-        System.out.println("step 6: init the tensor proto begin...");
+    @RequestMapping("fire")
+    public String fire() {
+        System.out.println("step 1: init the tensor proto begin...");
         // create TensorProto with 3 floats
         TensorProto.Builder tensorProtoBuilder = TensorProto.newBuilder();
         tensorProtoBuilder.setDtype(DataType.DT_FLOAT);
-        tensorProtoBuilder.addFloatVal(1.0f);
-        tensorProtoBuilder.addFloatVal(2.0f);
-        tensorProtoBuilder.addFloatVal(5.0f);
-        System.out.println("step 6: init the tensor proto successfully");
+        tensorProtoBuilder.addFloatVal(1);
+        tensorProtoBuilder.addFloatVal(1);
+        tensorProtoBuilder.addFloatVal(1);
 
-        System.out.println("step 7: init the tensor shape proto begin...");
+        System.out.println("step 1: init the tensor proto successfully");
+
+        System.out.println("step 2: init the tensor shape proto begin...");
         // create TensorShapeProto
         TensorShapeProto.Builder tensorShapeBuilder = TensorShapeProto.newBuilder();
-        tensorShapeBuilder.addDim(TensorShapeProto.Dim.newBuilder().setSize(3));
-        System.out.println("step 7: init the tensor shape proto successfully");
+        tensorShapeBuilder.addDim(org.tensorflow.framework.TensorShapeProto.Dim.newBuilder().setSize(1));
+        tensorShapeBuilder.addDim(org.tensorflow.framework.TensorShapeProto.Dim.newBuilder().setSize(1));
+        System.out.println("step 2: init the tensor shape proto successfully");
 
 
-        System.out.println("step 8: set the shape for proto begin...");
+        System.out.println("step 3: set the shape for proto begin...");
         // set shape for proto
         tensorProtoBuilder.setTensorShape(tensorShapeBuilder.build());
-        System.out.println("step 8: set the shape for proto successfully");
+        System.out.println("step 3: set the shape for proto successfully");
 
-        System.out.println("step 9: init the proto begin...");
-        // build proto
-        TensorProto proto = tensorProtoBuilder.build();
-        System.out.println("step 9: init the proto successfully");
-
-        System.out.println("step 10: init the request x begin...");
-        // set proto for request
-        requestBuilder.putInputs("x", proto);
-        System.out.println("step 10: init the request x successfully");
-
-        System.out.println("step 11: build the request begin...");
+        System.out.println("step 4: build the request begin...");
         // build request
         Predict.PredictRequest request = Predict
                 .PredictRequest
                 .newBuilder()
                 .setModelSpec(modelSpecBuilder)
-                .putInputs("x", proto)
+                .putInputs("avg_playtime_15d", tensorProtoBuilder.build())
                 .build();
 
         System.out.println("Printing request \n" + request.toString());
-        System.out.println("step 11: build the request successfully");
+        System.out.println("step 4: build the request successfully");
 
-        System.out.println("step 12: init the predict begin...");
+        System.out.println("step 5: init the predict begin...");
         // run predict
         Predict.PredictResponse response = stub.predict(request);
         System.out.println(response.toString());
-        System.out.println("step 12: fire the predict successfully");
+        System.out.println("step 5: fire the predict successfully");
 
         return "all done!";
     }
